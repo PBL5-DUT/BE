25e0a9a2f66511dcb844b883ed276e995e25fb2d
alutoday <anhnn1201@gmail.com>
2025-06-10 22:03:19 +0700

    tobe

--- src/main/java/com/pbl/pbl_be/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/pbl/pbl_be/service/impl/ProjectServiceImpl.java
@@ -192,10 +192,16 @@ public class ProjectServiceImpl implements ProjectService {
             }
             returnProjects.add(project);
         }
-
-        return returnProjects.stream()
+        List<ProjectDTO> projectDTOs = returnProjects.stream()
                 .map(project -> projectMapper.toDTO(project, userId))
                 .collect(Collectors.toList());
+        projectDTOs.removeIf(projectDTO ->
+                !projectDTO.getStatus().equals("approved") &&
+                        !projectDTO.getStatus().equals("finished") &&
+                        !projectDTO.getStatus().equals("locked")
+        );
+
+        return projectDTOs;
     }
 
     @Override
@@ -212,12 +218,11 @@ public class ProjectServiceImpl implements ProjectService {
                 .map(project -> projectMapper.toDTO(project, userId))
                 .collect(Collectors.toList());
 
-        // Loại bỏ các dự án có trạng thái KHÓA, ĐANG CHỜ DUYỆT, HOÀN THÀNH
-        // Nếu mục đích của "joinedProjects" là chỉ hiển thị các dự án đang hoạt động
+
         projectDTOs.removeIf(projectDTO ->
-                projectDTO.getStatus().equals("locked") ||
-                        projectDTO.getStatus().equals("pending") || // Thêm trạng thái pending
-                        projectDTO.getStatus().equals("finished")
+                !projectDTO.getStatus().equals("approved") &&
+                        !projectDTO.getStatus().equals("finished") &&
+                        !projectDTO.getStatus().equals("locked")
         );
         return projectDTOs;
 
@@ -236,7 +241,6 @@ public class ProjectServiceImpl implements ProjectService {
     @Override
     @Transactional // Để đảm bảo giao dịch
     @CacheEvict(value = {"projectDetails", "allProjects", "projectsByStatus", "projectsRemaining", "projectsLiked"}, allEntries = true)
-    // Xóa các cache liên quan đến lượt thích
     public void likeProject(Integer projectId, Integer userId) {
         if (this.projectLikeRepo.existsByProject_ProjectIdAndUser_Id(projectId, userId)) {
             this.projectLikeRepo.deleteByProject_ProjectIdAndUser_Id(projectId, userId);
